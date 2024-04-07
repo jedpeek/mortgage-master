f56c7b02c5f3770bbc3efe1e357cf7ba4df748d6
jed peek <jedpeek@MacBook-Pro-3.local>
2024-04-07 12:03:38 -0700

    update

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,14 @@
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL!;
+
 interface MongooseConn {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
-let cached: any = (global as any).mongoose;
+
+let cached: MongooseConn = (global as any).mongoose;
+
 if (!cached) {
   cached = (global as any).mongoose = {
     conn: null,
@@ -15,13 +18,15 @@ if (!cached) {
 
 export const connect = async () => {
   if (cached.conn) return cached.conn;
+
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
       dbName: "mortgage-master",
       bufferCommands: false,
-      connectTimeoutMS: 30000,
+      connectTimeoutMS: 50000,
     });
+
   cached.conn = await cached.promise;
 
   return cached.conn;
