940918f110d2c7bd718a0e13e49fc0ed49a0775a
jed peek <jedpeek@MacBook-Pro-3.local>
2024-04-09 20:07:39 -0700

    changed switch statement in stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
       const checkoutSessionAsyncPaymentSucceeded = event.data.object;
 
       break;
-    case "checkout.session.completed":
+    case "payment_intent.succeeded":
       const checkoutSessionCompleted: any = event.data.object;
       console.log("WE DID IT: ", checkoutSessionCompleted);
       addOrderToUser(checkoutSessionCompleted);
